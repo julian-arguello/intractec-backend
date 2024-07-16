a9f67f900a2578519795399de65bb213a44dcaa2
julian <julian.andres.arguello@gmail.com>
2024-07-16 00:30:53 -0300

    modificando msj de error

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ export function login(req, res) {
             const user = await usersDao.findByEmail(entity.email)
             console.log('login controller user', user);
             if (user) {
-                if (user.softDelete != 'true') {
+                if (user.softDelete !== 'true') {
                     user.role = await rolesDao.findById(user.role_id)
                     const validate = await bcrypt.compare(entity.password, user.password)
                     if (validate) {
@@ -31,19 +31,20 @@ export function login(req, res) {
                         console.log('login controller token', token);
                         res.header('auth-token', token).json({ user: user, token: token })
                     } else {
-                        return res.status(401).json({ err: 401, 'status': 'error', msg: "El password no coincide." })
+                        return res.status(401).json({ err: 401, status: 'error', msg: "Los datos proporcionados no son válidos." })
                     }
                 } else {
-                    return res.status(400).json({ error: 400, 'status': 'error', msg: "EL usuario esta desactivado" })
+                    return res.status(400).json({ error: 400, status: 'error', msg: "El usuario está desactivado." })
                 }
             } else {
-                return res.status(400).json({ err: 401, 'status': 'error', msg: "El email no existe." })
+                return res.status(400).json({ err: 400, status: 'error', msg: "Los datos proporcionados no son válidos." })
             }
         })
         .catch((err) => {
-            res.status(500).json({ err: 500, msg: "Error al validar", 'status': 'error', validateError: err.errors })
+            res.status(500).json({ err: 500, msg: "Los datos proporcionados no son válidos.", status: 'error', validateError: err.errors })
         })
 }
+
 /*-------------------------------------------------------------------------------------------*/
 export function recovery(req, res) {
     //console.log("recovery:", req.body)
